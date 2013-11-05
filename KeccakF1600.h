cd97a60d901f5d1216074e6ab5d8fa246cfb49de
Siddharth Rao <sidtechnical@gmail.com>
2013-11-05 06:55:40 +0200

    Update KeccakF1600.h
    Adding function definitions from .c File

--- KeccakF1600.h
+++ KeccakF1600.h
@@ -3,5 +3,11 @@
  */
 
 unsigned long** KeccakF(unsigned long** INPUT);
+/*Function Definations */
+unsigned long** rounds(unsigned long**,unsigned long);
+unsigned long** theta_step(unsigned long**);
+unsigned long** pi_step(unsigned long**);
+unsigned long** x_step(unsigned long** ,unsigned long**);
+unsigned long rot(unsigned long, unsigned int);
 
 
